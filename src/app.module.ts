938469d0a79a97deea42cfb0a2c3563f4b261159
Samuel Parker <samuel.a.parker00@gmail.com>
2024-10-09 12:00:10 -0400

    install express types and add CRUD recipe for plants

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,10 @@ import { DogsController } from './dogs/dogs.controller';
 import { DogsService } from './dogs/dogs.service';
 import { DogsModule } from './dogs/dogs.module';
 import { CatsService } from './cats/cats.service';
+import { PlantsModule } from './plants/plants.module';
 
 @Module({
-  imports: [CatsModule, DogsModule],
+  imports: [CatsModule, DogsModule, PlantsModule],
   controllers: [AppController, CatsController, DogsController],
   providers: [AppService, DogsService, CatsService],
 })
