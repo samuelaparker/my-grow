9f3890377dd0966124a9029277232da637d52963
Samuel Parker <samuel.a.parker00@gmail.com>
2024-10-09 12:05:13 -0400

    add post to cats controller as an example

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { CatsService } from './cats.service';
 
 @Controller('cats')
 export class CatsController {
   constructor(private readonly CatsService: CatsService) {}
+
+  @Post()
+  create(): string {
+    return 'this action adds a new cat';
+  }
+
   @Get()
   getCats(): string {
     return this.CatsService.getCats();
