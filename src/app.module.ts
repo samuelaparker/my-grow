3324d152b8fe769322b6525042c9b6211adcec43
Samuel Parker <samuel.a.parker00@gmail.com>
2024-10-10 07:51:51 -0400

    add PlantsController to controller array in App module

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,10 +8,16 @@ import { DogsService } from './dogs/dogs.service';
 import { DogsModule } from './dogs/dogs.module';
 import { CatsService } from './cats/cats.service';
 import { PlantsModule } from './plants/plants.module';
+import { PlantsController } from './plants/plants.controller';
 
 @Module({
   imports: [CatsModule, DogsModule, PlantsModule],
-  controllers: [AppController, CatsController, DogsController],
+  controllers: [
+    AppController,
+    CatsController,
+    DogsController,
+    PlantsController,
+  ],
   providers: [AppService, DogsService, CatsService],
 })
 export class AppModule {}
